a7761c969a6b5bbaa6c789d9aa966d989a0df537
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-07-30 22:03:21 +0300

    Addede the "download_img" and "search_full_name_img" functions.

---
+++ downloader.py
@@ -0,0 +1,25 @@
+import os
+import urllib.parse
+from pathlib import Path
+
+import requests
+
+
+def download_img(img_url: str, save_path: str):
+    Path(save_path).mkdir(parents=True, exist_ok=True)
+    img_name = search_full_name_image(img_url)
+
+    response = requests.get(img_url)
+    response.raise_for_status()
+
+    with open(f"{save_path}/{img_name}", 'wb') as file:
+        file.write(response.content)
+
+
+def search_full_name_img(url: str) -> str:
+    img_url = urllib.parse.urlsplit(url).path
+    unquote_img_url = urllib.parse.unquote(img_url)
+    img_file = os.path.split(unquote_img_url)[-1]
+    name_img = "".join(os.path.splitext(img_file))
+
+    return name_img
